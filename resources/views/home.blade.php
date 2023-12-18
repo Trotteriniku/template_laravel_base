c940977939d82c14e0d574a50b801beb3a0a43c7
Trotteriniku <stefanozidda3@gmail.com>
2023-12-18 12:24:36 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title, home')
+
+@section('content')
+<main>
+    <div class=" container ">
+        <h1>Home Page</h1>
+    </div>
+</main>
+
+@endsection('content')
